c3c437611c91b221ab25e11700f299ddd4698194
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-10-12 23:09:14 -0400

    Added face detection

--- fake_faces/data_loader.py
+++ fake_faces/data_loader.py
@@ -2,6 +2,17 @@ import os
 import cv2
 import numpy as np
 
+import face_recognition as fr
+
+def detect_face(img):
+    ### Returns the face crop ###
+    face_locations = fr.face_locations(img)[0] # only take first face
+    top, right, bottom, left = face_locations
+
+    face = img[top:bottom, left:right]
+
+    return face
+
 ### Load data from folder ###
 ### resize images to a specific dimensions ###
 def data_from_dir(data_dir, dimensions=(128,128,1), batch_size=32, max_num_img=None):
@@ -18,6 +29,7 @@ def data_from_dir(data_dir, dimensions=(128,128,1), batch_size=32, max_num_img=N
 
             abs_path = dir + "/" + file
             img = cv2.imread(abs_path)
+            img = detect_face(img)
             img = cv2.resize(img, (dimensions[0], dimensions[1]))
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img = img.reshape(dimensions[0], dimensions[1], dimensions[2])
