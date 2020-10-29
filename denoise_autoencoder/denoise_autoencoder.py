bd5640033af0275027f81433dfbbfbce45ee880b
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-10-29 20:58:31 +0700

    ahihi

--- denoise_autoencoder/denoise_autoencoder.py
+++ denoise_autoencoder/denoise_autoencoder.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model
 from tensorflow.keras import optimizers
-from tensorflow.keras import regularizers
 from tensorflow.keras import backend as K
+from tensorflow.keras.regularizers import l2
 
 class DenoiseAutoencoder():
     def __init__(self, input_shape=(128, 128, 3), encoding_dim=32, lambda_=1e-4):
@@ -32,23 +32,35 @@ class DenoiseAutoencoder():
         x = Input(shape=self.input_shape)
         
         # Encoder
-        e_conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+        e_conv1 = Conv2D(64, (3, 3), activation='relu', padding='same', 
+                kernel_regularizer=l2(self.lambda_/2))(x)
         pool1 = MaxPooling2D((2, 2), padding='same')(e_conv1)
         batchnorm_1 = BatchNormalization()(pool1)
-        e_conv2 = Conv2D(32, (3, 3), activation='relu', padding='same')(batchnorm_1)
+        
+        e_conv2 = Conv2D(32, (3, 3), activation='relu', padding='same',
+                kernel_regularizer=l2(self.lambda_/2))(batchnorm_1)
         pool2 = MaxPooling2D((2, 2), padding='same')(e_conv2)
         batchnorm_2 = BatchNormalization()(pool2)
-        e_conv3 = Conv2D(16, (3, 3), activation='relu', padding='same')(batchnorm_2)
+        
+        e_conv3 = Conv2D(16, (3, 3), activation='relu', padding='same',
+                kernel_regularizer=l2(self.lambda_/2))(batchnorm_2)
         h = MaxPooling2D((2, 2), padding='same')(e_conv3)
 
 
         # Decoder
-        d_conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(h)
+        d_conv1 = Conv2D(64, (3, 3), activation='relu', padding='same',
+                kernel_regularizer=l2(self.lambda_/2))(h)
         up1 = UpSampling2D((2, 2))(d_conv1)
-        d_conv2 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1)
+        
+        d_conv2 = Conv2D(32, (3, 3), activation='relu', padding='same',
+                kernel_regularizer=l2(self.lambda_/2))(up1)
         up2 = UpSampling2D((2, 2))(d_conv2)
-        d_conv3 = Conv2D(16, (3, 3), activation='relu', padding='same')(up2)
+        
+        d_conv3 = Conv2D(16, (3, 3), activation='relu', padding='same',
+                kernel_regularizer=l2(self.lambda_/2))(up2)
         up3 = UpSampling2D((2, 2))(d_conv3)
+        
+        ### One last Conv to restore original depth of image ###
         r = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(up3)
 
         model = Model(x, r)
