6e698c5eb6522514a7bd428f2c9f9bf1d1a15b0c
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-11-14 17:30:26 +0700

    ahihi

---
+++ variational_autoencoder/model.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.keras.layers import *
+from tensorflow.keras import backend as K
+from tensorflow.keras import regularizers
+from tensorflow.keras import optimizers
