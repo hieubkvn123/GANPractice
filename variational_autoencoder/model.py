7345293433c19815d1b2f046ab98aba068c8a9b5
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-11-14 17:33:52 +0700

    ahihi

--- variational_autoencoder/model.py
+++ variational_autoencoder/model.py
@@ -6,3 +6,11 @@ from tensorflow.keras.layers import *
 from tensorflow.keras import backend as K
 from tensorflow.keras import regularizers
 from tensorflow.keras import optimizers
+
+class VAE(object):
+    def __init__(self, latent_dim=256, input_shape=(28,28,1)):
+        self.latent_dim = latent_dim
+        self.input_shape = input_shape
+
+    def build(self):
+
