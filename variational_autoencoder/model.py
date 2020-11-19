d784779997c1b8a572988d69fb903fe2baabf236
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-11-19 23:52:13 +0700

    ahihi

--- variational_autoencoder/model.py
+++ variational_autoencoder/model.py
@@ -13,6 +13,24 @@ class VAE(object):
         self.latent_dim = latent_dim
         self.input_shape = input_shape
 
+    def decoder(self, name='decoder'):
+        decoder_input = Input(shape=(self.latent_dim,))
+        dense1 = Dense(7 * 7 * 16, activation='relu', name='decoder_input')(decoder_input)
+        reshape = Reshape(target_shape=(7,7,16))(dense1)
+        
+        ### Now tensor size = 14 x 14 x 16 ###
+        conv1 = Conv2D(16, kernel_size=(3,3), activation='relu', padding='same')(reshape)
+        up1 = UpSampling2D((2,2))(conv1)
+
+        ### Now tensor size = 28 x 28 x 8 ###
+        conv2 = Conv2D(8, kernel_size=(3,3), activation='relu', padding='same')(up1)
+        up2 = UpSampling2D((2,2))(conv2)
+
+        decoder_output = Conv2D(1, kernel_size=(3,3), activation='sigmoid', padding='same')(up2)
+        decoder = Model(decoder_input, decoder_output, name=name)
+
+        return decoder 
+
     def build(self):
         inputs = Input(shape=self.input_shape)
         
@@ -29,7 +47,7 @@ class VAE(object):
         flatten = Flatten()(pool2)
         z_mu = Dense(self.latent_dim, activation='sigmoid', name='encoded_mean')(flatten)
         z_log_var = Dense(self.latent_dim, activation='sigmoid', name='encoded_logvar')(flatten)
-        z_sigma = Lambda(lambda x : K.exp(z_log_var) ** 0.5 )(z_log_var)
+        z_sigma = Lambda(lambda x : K.exp(x) ** 0.5 )(z_log_var)
 
         ### Reparameterize : z = mu + sigma * epsilon ###
         ### eps ~ N(1, 0) ###
@@ -37,40 +55,37 @@ class VAE(object):
         z = Multiply()([z_sigma, z])
         z = Add(name='latent_input')([z, z_mu])
 
-        dense1 = Dense(7 * 7 * 16, activation='relu', name='decoder_input')(z)
-        reshape = Reshape(target_shape=(7,7,16))(dense1)
-        
-        ### Now tensor size = 14 x 14 x 16 ###
-        conv1 = Conv2D(16, kernel_size=(3,3), activation='relu', padding='same')(reshape)
-        up1 = UpSampling2D((2,2))(conv1)
+        ### Build the decoder ###
+        decoder = self.decoder(name='decoder')
+        output  = decoder(z)
 
-        ### Now tensor size = 28 x 28 x 8 ###
-        conv2 = Conv2D(8, kernel_size=(3,3), activation='relu', padding='same')(up1)
-        up2 = UpSampling2D((2,2))(conv2)
+        ### Rename output tensor ###
+        decoded_output = Lambda(lambda x : x, name='decoded_output')(output) 
+        encoded_output = Concatenate(axis=1, name='encoded_output')([z_mu, z_log_var])
 
-        output = Conv2D(1, kernel_size=(3,3), activation='sigmoid', padding='same')(up2)
-        model = Model(inputs, [z_mu, z_log_var, output])
+        model = Model(inputs=inputs, outputs=[decoded_output, encoded_output]) # [z_mu, z_log_var, output])
         adam = optimizers.Adam(lr=1e-3, amsgrad=True)
-        model.compile(optimizer=adam, loss=self.loss_function)
+
+        losses = {
+                'decoded_output' : tf.keras.losses.mean_squared_error,
+                'encoded_output' : self.kl_divergence_loss
+        }
+        model.compile(optimizer=adam, loss=losses)
 
         return model
 
-    def loss_function(y_true, y_pred):
-        mu, log_var, output = y_pred
-
+    def kl_divergence_loss(self, y_true, y_pred):
+        mu, log_var = tf.split(y_pred, num_or_size_splits=2, axis=1)
+        print(mu.shape, log_var.shape)
 
         ''' The formula is calculate KL divergence of two gaussian distributions '''
-        kl_divergence = -0.5 * K.sum(1 + log_var -
+        kl_divergence = -0.5 * tf.reduce_sum(1 + log_var -
                                     K.square(mu) - 
                                     K.exp(log_var), axis=-1)
-        kl_divergence = K.mean(kl_divergence)
 
-        reconstruction_loss = (output - y_true) ** 2
-        reconstruction_loss = K.mean(recontruction_loss)
+        kl_divergence = tf.reduce_mean(kl_divergence)
 
-        loss = reconstruction_loss + kl_divergence
-
-        return loss 
+        return kl_divergence 
 
 
 
--- variational_autoencoder/train_mnist.py
+++ variational_autoencoder/train_mnist.py
@@ -22,14 +22,39 @@ BATCH_SIZE = int(args['batch_size'])
 NUM_TRAIN = int(args['num_train'])
 MODEL_CHECKPOINT = args['checkpoint']
 
-net = VAE().build()
+model= VAE().build()
 
 if(os.path.exists(MODEL_CHECKPOINT)):
-    net.load_weights(MODEL_CHECKPOINT)
+    print('[*] Loading pretrained model ... ')
+    model.load_weights(MODEL_CHECKPOINT)
 
 ### Load dataset ###
 (X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.mnist.load_data()
-X_train = X_train.reshape(-1, 28,28,1) / 255.
-X_test = X_test.reshape(-1, 28, 28, 1) / 255.
+X_train = (X_train.reshape(-1, 28,28,1) / 255.0).astype('float32')
+X_test = (X_test.reshape(-1, 28, 28, 1) / 255.0).astype('float32')
 
+print('[*] Number of training images : {}'.format(X_train.shape[0]))
+print('[*] Number of testing images : {}'.format(X_test.shape[0]))
 
+callbacks = [
+    ModelCheckpoint(MODEL_CHECKPOINT, save_best_only=True, verbose=1),
+    EarlyStopping(patience=15, verbose=1),
+    CSVLogger('training.log.csv')
+]
+
+Y = {
+    'decoded_output' : X_train,
+    'encoded_output' : np.ones((X_train.shape[0], 512))
+}
+
+Y_ = {
+    'decoded_output' : X_test,
+    'encoded_output' : np.ones((X_test.shape[0], 512))
+}
+
+print(model.summary())
+model.fit(X_train, Y,
+        callbacks=callbacks,
+        validation_data=(X_test, Y_),
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE)
